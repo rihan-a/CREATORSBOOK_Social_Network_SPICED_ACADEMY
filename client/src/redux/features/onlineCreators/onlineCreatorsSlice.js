322930daf6862757f3638c6859abb7c0901baa99
rihan_a <a.rihan@live.com>
2022-12-18 22:41:36 +0100

    created onlineCreators Slice

---
+++ client/src/redux/features/onlineCreators/onlineCreatorsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const onlineCreatorsSlice = createSlice({
+    name: "onlineCreators",
+    initialState: {
+        creatorsDataList: []
+    },
+    // action creators
+    reducers: {
+        online: (state, action) => {
+            state.creatorsDataList = action.payload;
+            console.log(state.creatorsDataList);
+        },
+        offline: (state, action) => {
+            // remove disconnected creator from the DataList
+            state.creatorsDataList = state.creatorsDataList.filter((creator) => creator.id != action.payload);
+        },
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const { online, offline } = onlineCreatorsSlice.actions;
+export default onlineCreatorsSlice.reducer;
+
+
+
+
