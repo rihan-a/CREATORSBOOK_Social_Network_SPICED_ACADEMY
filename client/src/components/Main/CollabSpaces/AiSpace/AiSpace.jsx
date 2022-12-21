c35378477402ea0dfee14193b147efcacf5a725c
rihan_a <a.rihan@live.com>
2022-12-21 22:47:50 +0100

    add title and counter limit

--- client/src/components/Main/CollabSpaces/AiSpace/AiSpace.jsx
+++ client/src/components/Main/CollabSpaces/AiSpace/AiSpace.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./AiSpace.css";
 import { ThreeDots } from "react-loader-spinner";
 
@@ -6,6 +6,8 @@ function AiSpace() {
     const [creatorPrompt, setCreatorPrompt] = useState("");
     const [resultUrl, setResultUrl] = useState();
     const [loading, setLoadingState] = useState(false);
+    const [count, setCount] = useState(0);
+    const [error, setError] = useState("");
 
     const addPromptTxt = (e) => {
         setCreatorPrompt(e.target.value);
@@ -23,23 +25,45 @@ function AiSpace() {
                 console.log(res);
                 setLoadingState(false);
                 setResultUrl(res.url);
+                setCount(res.count);
+                if (res.error) {
+                    setError(res.error);
+                }
             })
             .catch((err) => {
                 console.log("error here :(", err);
             });
     };
 
+    useEffect(() => {
+        fetch(`/api/ai/count`)
+            .then((result) => result.json())
+            .then((result) => {
+                console.log(result.count.count);
+                if (result.count.count) {
+                    setCount(result.count.count);
+                } else {
+                    setCount(0);
+                }
+            });
+    }, []);
+
     return (
         <div className="collabspace-ai-container">
+            <h3>Image generation using AI (TEXT TO IMAGE)</h3>
+            <p>
+                Since it's a beta version, There is a limit of 10 prompts per
+                user. ({count}/10) <span className="error">{error}</span>
+            </p>
             <div className="ai-prompt-txtarea">
-                <h3>Write your prompt here</h3>
                 <textarea
                     name="creatorPrompt"
                     id="creatorPrompt"
-                    cols="30"
-                    rows="10"
                     value={creatorPrompt}
                     onChange={addPromptTxt}
+                    placeholder="Write your prompt here"
+                    autoCorrect="on"
+                    autoComplete="on"
                 ></textarea>
                 <p className="bio-btn" onClick={sendCreatorPrompt}>
                     Create
@@ -47,23 +71,30 @@ function AiSpace() {
             </div>
             <div className="ai-output-container">
                 <div className="ai-img-container">
+                    <div className="ai-image-notes">
+                        <p>Generated Image will appear here</p>
+                        <p>
+                            Image link will expire in one hour, if you would
+                            like to keep your image please save it.
+                        </p>
+                    </div>
                     <img src={resultUrl} alt="" />
+                    {loading == true && (
+                        <div className="loading-spinner">
+                            <ThreeDots
+                                height="150"
+                                width="150"
+                                radius="9"
+                                color="var(--accentColor)"
+                                ariaLabel="three-dots-loading"
+                                wrapperStyle={{}}
+                                wrapperClassName=""
+                                visible={true}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
-            {loading == true && (
-                <div className="loading-spinner">
-                    <ThreeDots
-                        height="120"
-                        width="120"
-                        radius="9"
-                        color="var(--accentColor)"
-                        ariaLabel="three-dots-loading"
-                        wrapperStyle={{}}
-                        wrapperClassName=""
-                        visible={true}
-                    />
-                </div>
-            )}
         </div>
     );
 }
