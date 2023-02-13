bb4ca116ef2187ff3c157d85dd8f258f006b3938
rihan_a <a.rihan@live.com>
2023-02-13 13:46:16 +0100

    remove registration endpoint and use router instead

--- server/server.js
+++ server/server.js
@@ -119,66 +119,11 @@ app.get("/user/id", (req, res) => {
 });
 
 
-// import registeration rout
-const registration = require("./routes/registeration");
-// connect to rout
-app.use("/register", registration);
+// import registeration route
+const registration = require("./routes/registration");
+app.use(registration);
 
 
-// Registration Route start ---------------------------------------------------->
-//------------------------------------------------------------------------------>
-//POST
-// app.post("/register", (req, res) => {
-//     //read data sent by the user in the form!
-//     const { firstName, lastName, email, password } = req.body;
-
-//     if (firstName == undefined || lastName == undefined || email == undefined || password == undefined ||
-//         firstName.trim() == "" ||
-//         lastName.trim() == "" ||
-//         email.trim() == "" ||
-//         password.trim() == ""
-//     ) {
-//         return res.json({
-//             error: "Something went wrong, please fill your data properly!"
-//         });
-//     }
-
-//     // check if the user email is already exsiting -------------------------
-//     getCreatorByEmail(email).then((user) => {
-//         //console.log({ user });
-//         if (user) {
-//             return res.json({
-//                 error: "This email is already registered!",
-//             });
-
-//         }
-
-//         // Hashing password before saving it to data base -----------------
-//         const hashedPassword = bcrypt.hashSync(password, salt);
-//         // -----------------------------------------------------------------
-
-//         let now = new Date();
-//         // create a new profile --------------------------------------------
-//         createUser({
-//             firstName: firstName,
-//             lastName: lastName,
-//             email: email,
-//             password: hashedPassword,
-//             createdAt: now,
-//         })
-//             .then((result) => {
-//                 //console.log("created profile", result);
-//                 console.log(firstName + " " + lastName);
-//                 req.session.userID = result.id;
-//                 req.session.userName = { firstName, lastName, email };
-//                 req.session.logedIn = true;
-//                 return res.json({ success: true });
-//             })
-//             .catch((error) => {
-//                 console.log(error);
-//             });
-//     });
-// });
 
 // Log-In Route ------------------------------------------------------------>
 //-------------------------------------------------------------------------->
