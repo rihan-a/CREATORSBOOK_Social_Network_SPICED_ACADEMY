cd3050452ae23c6d0fa92b3d69cc83ec8c354076
rihan_a <a.rihan@live.com>
2023-02-14 14:19:41 +0100

    remove unused props

--- client/src/components/Main/Profile/BioEditor/BioEditor.jsx
+++ client/src/components/Main/Profile/BioEditor/BioEditor.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function BioEditor(props) {
+function BioEditor() {
     const [bioState, setBioState] = useState("add");
 
     const [bioText, setBioText] = useState("");
