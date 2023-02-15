192f2a73f53b41535a6952c0a268bb02fe699ca9
rihan_a <a.rihan@live.com>
2023-02-15 17:54:37 +0100

    update Login component to functional component

--- client/src/components/Welcome/Registration-Login/Login.jsx
+++ client/src/components/Welcome/Registration-Login/Login.jsx
@@ -1,80 +1,71 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./RegistrationLogin.css";
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: "",
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
 
-    handleInputChange(e) {
-        //console.log(e);
-        const text = e.currentTarget.value;
-        this.setState({
-            [e.currentTarget.name]: text,
-        });
-    }
+function Login() {
+    const [userInput, setUserInput] = useState({ email: "", password: "" });
 
-    handleSubmit() {
-        console.log("About to submit the form!");
-        //console.log(this.state);
+    const [error, setError] = useState("");
+
+    const handleInputChange = (e) => {
+        setUserInput((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = () => {
         fetch("/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(this.state),
+            body: JSON.stringify(userInput),
         })
             .then((res) => res.json())
             .then((response) => {
-                console.log({ response });
                 if (response.success == true) {
                     window.location.replace("/");
                 } else {
-                    this.setState({ error: response.error });
+                    setError(response.error);
                 }
             })
             .catch((err) => {
                 console.log("error", err);
             });
-    }
+    };
 
-    render() {
-        return (
-            <div className="signup-signin-components">
-                <h2>Welcome back!</h2>
-                <div className="signup-signin-form login">
-                    <input
-                        type="email"
-                        name="email"
-                        onChange={this.handleInputChange}
-                        placeholder="Email"
-                    ></input>
+    return (
+        <div className="signup-signin-components">
+            <h2>Welcome back!</h2>
+            <div className="signup-signin-form login">
+                <input
+                    type="email"
+                    name="email"
+                    onChange={handleInputChange}
+                    placeholder="Email"
+                ></input>
 
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={this.handleInputChange}
-                        placeholder="Password"
-                    ></input>
-                    <input
-                        type="submit"
-                        value="Log-In"
-                        onClick={(e) => this.handleSubmit(e)}
-                    ></input>
-                    <span>
-                        Not a member yet ?<Link to="/">Sign up!</Link>
-                    </span>
-                    <span>
-                        Forgot password?
-                        <Link to="/password/reset">Reset password</Link>
-                    </span>
-                    <span className="error">{this.state.error}</span>
-                </div>
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleInputChange}
+                    placeholder="Password"
+                ></input>
+                <input
+                    type="submit"
+                    value="Log-In"
+                    onClick={handleSubmit}
+                ></input>
+                <span>
+                    Not a member yet ?<Link to="/">Sign up!</Link>
+                </span>
+                <span>
+                    Forgot password?
+                    <Link to="/password/reset">Reset password</Link>
+                </span>
+                <span className="error">{error}</span>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Login;
