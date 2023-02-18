e12b7aec49fb2b4de2e153bf514f0e2cd824af89
rihan_a <a.rihan@live.com>
2023-02-18 19:29:59 +0100

    refactor Registration component to functional component

--- client/src/components/Welcome/Registration-Login/Registration.jsx
+++ client/src/components/Welcome/Registration-Login/Registration.jsx
@@ -1,98 +1,92 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./RegistrationLogin.css";
 
-class Registration extends Component {
-    constructor(props) {
-        super(props);
+function Registration() {
+    const [userInput, setUserInput] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
 
-        this.state = {
-            error: "",
-        };
+    const [error, setError] = useState("");
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+    const handleInputChange = (e) => {
+        setUserInput((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
 
-    handleInputChange(e) {
-        //console.log(e);
-        const text = e.currentTarget.value;
-        this.setState({
-            [e.currentTarget.name]: text,
-        });
-    }
-
-    handleSubmit() {
+    const handleSubmit = () => {
         fetch("/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(this.state),
+            body: JSON.stringify(userInput),
         })
             .then((res) => res.json())
             .then((response) => {
                 if (response.success == true) {
                     location.reload();
                 } else {
-                    this.setState({
-                        error: response.error,
-                    });
+                    setError(response.error);
                 }
             })
             .catch((err) => {
                 console.log("error", err);
             });
-    }
+    };
 
-    render() {
-        return (
-            <div className="signup-signin-components">
-                <h2>
-                    Create an account and be a part of what's next in art,
-                    design, fashion, web culture & more.
-                </h2>
+    return (
+        <div className="signup-signin-components">
+            <h2>
+                Create an account and be a part of what's next in art, design,
+                fashion, web culture & more.
+            </h2>
 
-                <div className="signup-signin-form">
-                    <input
-                        type="text"
-                        name="firstName"
-                        onChange={this.handleInputChange}
-                        placeholder="First name"
-                    ></input>
+            <div className="signup-signin-form">
+                <input
+                    type="text"
+                    name="firstName"
+                    onChange={handleInputChange}
+                    placeholder="First name"
+                ></input>
 
-                    <input
-                        type="text"
-                        name="lastName"
-                        onChange={this.handleInputChange}
-                        placeholder="Last name"
-                    ></input>
+                <input
+                    type="text"
+                    name="lastName"
+                    onChange={handleInputChange}
+                    placeholder="Last name"
+                ></input>
 
-                    <input
-                        type="email"
-                        name="email"
-                        onChange={this.handleInputChange}
-                        placeholder="Email"
-                    ></input>
+                <input
+                    type="email"
+                    name="email"
+                    onChange={handleInputChange}
+                    placeholder="Email"
+                ></input>
 
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={this.handleInputChange}
-                        placeholder="Password"
-                    ></input>
-                    <input
-                        type="submit"
-                        value="Register Now"
-                        onClick={(e) => this.handleSubmit(e)}
-                    ></input>
-                    <span>
-                        Already a member ? <Link to="/login">Log in!</Link>
-                    </span>
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleInputChange}
+                    placeholder="Password"
+                ></input>
+                <input
+                    type="submit"
+                    value="Register Now"
+                    onClick={handleSubmit}
+                ></input>
+                <span>
+                    Already a member ? <Link to="/login">Log in!</Link>
+                </span>
 
-                    <span className="error">{this.state.error}</span>
-                </div>
+                <span className="error">{error}</span>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Registration;
