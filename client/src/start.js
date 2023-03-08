664c9a3cb89ff494486c25cbd640711a0ea61e64
rihan_a <a.rihan@live.com>
2023-03-08 21:19:21 +0100

    update checkUserId fetch with async/await and use browserRouter

--- client/src/start.js
+++ client/src/start.js
@@ -3,25 +3,33 @@ import Welcome from "./components/Welcome/Welcome";
 import Main from "./components/Main/Main";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
 import { init } from "./socket";
 
-
 const root = createRoot(document.querySelector("main"));
 
-fetch("/user/id")
-    .then((response) => response.json())
-    .then((data) => {
+const checkUserId = async () => {
+    try {
+        const response = await fetch("/user/id");
+        const data = await response.json();
         // the userId comes from the session on the server!
-        // this means: the user is currently signed in!
         if (data.userID) {
+            // this means: the user is currently signed in!
             init(store);
             root.render(
                 <Provider store={store}>
-                    <Main />
+                    <BrowserRouter>
+                        <Main />
+                    </BrowserRouter>
                 </Provider>
             );
         } else {
             root.render(<Welcome />);
         }
-    });
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+checkUserId();
